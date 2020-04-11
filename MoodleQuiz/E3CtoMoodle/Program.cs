0176abbefb86f71a62a8df7f9c152aef9ae625f2
Fabrice Foray <fabrice.foray@hotmail.fr>
2020-04-11 12:10:14 +0200

    Traitement par Lot

--- MoodleQuiz/E3CtoMoodle/Program.cs
+++ MoodleQuiz/E3CtoMoodle/Program.cs
@@ -18,7 +18,7 @@ namespace E3CtoMoodle
             {
                 AfficherAide("switch de commande inconnu.");
             }
-            if (!File.Exists(args[1]))
+            if ( (args[1] != "*") && !File.Exists(args[1]) )
             {
                 AfficherAide("Fichier Inconnu : " + args[1]);
             }
@@ -36,7 +36,7 @@ namespace E3CtoMoodle
             switch (args[0].ToLower())
             {
                 case "-c":
-                    Convertion(args[1], destFile);
+                    Conversion(args[1], destFile);
                     break;
                 case "-p":
                     Preparation(args[1], destFile);
@@ -46,17 +46,48 @@ namespace E3CtoMoodle
             //
         }
 
-        private static void Preparation(string v, string destFile)
+        private static void Conversion(string orgFile, string destFile)
         {
-            E3CTest test = new E3CTest();
-            test.Load(v);
+            Console.WriteLine("Traitement : " + orgFile);
             //
         }
 
-        private static void Convertion(string v, string destFile)
+        private static void Preparation(string orgFile, string destFile)
         {
-            E3CTest test = new E3CTest();
-            test.Load(v);
+            List<String> files = new List<string>();
+            if (orgFile == "*")
+            {
+                foreach (var file in Directory.GetFiles(System.AppDomain.CurrentDomain.BaseDirectory))
+                {
+                    string ext = Path.GetExtension(file).ToLower();
+                    if ( ( ext == ".docx") || ( ext == ".txt") )
+                    {
+                        files.Add(file);
+                    }
+                }
+            }
+            else
+            {
+                files.Add(orgFile);
+            }
+            //
+            foreach( String fullPath in files )
+            {
+                try
+                {
+                    E3CTest test = new E3CTest();
+                    Console.WriteLine("Traitement : " + Path.GetFileName(fullPath));
+                    test.Load(fullPath);
+                    Console.WriteLine("  --> Terminé.");
+                    //
+                    test.Save(Path.GetFileNameWithoutExtension(fullPath)  + ".e3c.txt");
+                    Console.WriteLine("  --> Sauvegardé.");
+                }
+                catch ( Exception ex )
+                {
+                    Console.WriteLine("Erreur : {0}", ex.Message);
+                }
+            }
             //
         }
 
@@ -67,10 +98,18 @@ namespace E3CtoMoodle
             Console.WriteLine();
             Console.WriteLine("Usage :");
             Console.WriteLine("Préparer : Ajoute le bloc Réponse à chaque Question s'il est manquant");
-            Console.WriteLine("E3CMoodle.exe -p fichierTexteE3C [fichierTexteE3C_Reponse]");
+            Console.WriteLine("E3CMoodle.exe -p fichierE3C_original [fichierE3C]");
+            Console.WriteLine("E3CMoodle.exe -p *"); 
+            Console.WriteLine("Si fichierE3C_original est un fichier .docx, le texte est extrait avant conversion.");
+            Console.WriteLine("Si * est utilisé, tous les fichiers .txt/.docx du dossier courant sont traités.");
+            Console.WriteLine("fichierE3C a le nom du fichier d'origine avec l'extension .e3c.txt");
             Console.WriteLine();
             Console.WriteLine("Convertir : Convertit un formulaire E3C en Quiz Moodle");
-            Console.WriteLine("E3CMoodle.exe -c fichierTexteE3C [fichierTexteE3C_Moodle]");
+            Console.WriteLine("E3CMoodle.exe -c fichierE3C [fichierE3C_XMLMoodle]");
+            Console.WriteLine("E3CMoodle.exe -c *");
+            Console.WriteLine("Le fichierE3C est un fichier texte, utilisé pour générer un Quiz Moodle en XML.");
+            Console.WriteLine("Si * est utilisé, tous les fichiers .e3c.txt du dossier courant sont traités.");
+            Console.WriteLine("fichierE3C_XMLMoodle a le nom du fichier d'origine avec l'extension .xml");
             Console.WriteLine();
             Console.WriteLine();
             Environment.Exit(-1);
